7a4da4610f74ed5e63dac4b8e53c4fc798609d45
Kahlil Whitfield <darkristy2002@gmail.com>
2021-05-12 21:37:37 -0400

    feat(hover-wave.tsx): add framer motion implementation

--- pages/hover-wave.tsx
+++ pages/hover-wave.tsx
@@ -3,29 +3,89 @@ import { Container, H } from "@/components/UI";
 import { NextPage } from "next";
 import { FC, Fragment, useCallback } from "react";
 import tw, { styled } from "twin.macro";
-import anime from "animejs";
+// import anime from "animejs";
+import { motion, useAnimation, AnimationControls } from "framer-motion";
 
 interface FakeLinkProps {
 	name: string;
 	index: number;
 }
 
-const FakeLink: FC<FakeLinkProps> = ({ name, index }) => {
+// Anime.js Implementation
+
+// const FakeLink: FC<FakeLinkProps> = ({ name, index }) => {
+// 	const LinkWrapper = styled.div(() => [tw`mx-8 h-12  overflow-hidden cursor-pointer`]);
+
+// 	const targets = [`.text${index} .letter`, `.text${index + 4} .letter`];
+// 	const classNames = [`text${index}`, `text${index + 4}`];
+
+// 	const animateLetters = useCallback((yValue) => {
+// 		targets.map((target) => {
+// 			anime.remove(target);
+// 			anime({
+// 				targets: target,
+// 				translateY: yValue,
+// 				easing: "cubicBezier(.6, .0135, .06, .9)",
+// 				duration: 700,
+// 				delay: (_, i) => 28 * i,
+// 			});
+// 		});
+// 	}, []);
+
+// 	const linkInner = (
+// 		<>
+// 			{name.split(" ").map((word, wordI) => (
+// 				<div key={`word-${word}-${wordI}`} tw="inline-block mr-4">
+// 					{Array.from(word).map((c, i) => (
+// 						<span key={i} tw="w-auto relative inline-block" className="letter">
+// 							{c === " " ? "\u00A0" : c}
+// 						</span>
+// 					))}
+// 				</div>
+// 			))}
+// 		</>
+// 	);
+
+// 	return (
+// 		<LinkWrapper
+// 			key={index}
+// 			onMouseEnter={(): void => animateLetters(-48)}
+// 			onMouseLeave={(): void => animateLetters(0)}
+// 		>
+// 			{classNames.map((className, i) => (
+// 				<Fragment key={i}>
+// 					<H priority={2} className={className} customStyles={tw`uppercase `}>
+// 						{linkInner}
+// 					</H>
+// 				</Fragment>
+// 			))}
+// 		</LinkWrapper>
+// 	);
+// };
+
+const MotionLink: FC<FakeLinkProps> = ({ name, index }) => {
 	const LinkWrapper = styled.div(() => [tw`mx-8 h-12  overflow-hidden cursor-pointer`]);
+	const MotionHeading = motion(H, { forwardMotionProps: true });
 
-	const targets = [`.text${index} .letter`, `.text${index + 4} .letter`];
-	const classNames = [`text${index}`, `text${index + 4}`];
+	const transition = { duration: 0.475, ease: [0.6, 0.125, -0.05, 0.9] };
 
-	const animateLetters = useCallback((yValue) => {
-		targets.map((target) => {
-			anime.remove(target);
-			anime({
-				targets: target,
-				translateY: yValue,
-				easing: "easeOutExpo",
-				duration: 1000,
-				delay: (_, i) => 40 * i,
-			});
+	const firstAnimation = useAnimation();
+	const secondAnimation = useAnimation();
+	const animations: AnimationControls[] = [firstAnimation, secondAnimation];
+
+	const letterContainerVariants = {
+		before: { transition: { staggerChildren: 0.03 } },
+		after: { transition: { staggerChildren: 0.03 } },
+	};
+
+	const letterVariants = {
+		before: { y: 0, transition: { ...transition } },
+		after: { y: -48, transition: { ...transition } },
+	};
+
+	const animateLetters = useCallback((variant) => {
+		animations.map((animation) => {
+			animation.start(variant);
 		});
 	}, []);
 
@@ -34,9 +94,9 @@ const FakeLink: FC<FakeLinkProps> = ({ name, index }) => {
 			{name.split(" ").map((word, wordI) => (
 				<div key={`word-${word}-${wordI}`} tw="inline-block mr-4">
 					{Array.from(word).map((c, i) => (
-						<span key={i} tw="w-auto relative inline-block" className="letter">
+						<motion.span key={i} variants={letterVariants} tw="w-auto relative inline-block">
 							{c === " " ? "\u00A0" : c}
-						</span>
+						</motion.span>
 					))}
 				</div>
 			))}
@@ -46,14 +106,19 @@ const FakeLink: FC<FakeLinkProps> = ({ name, index }) => {
 	return (
 		<LinkWrapper
 			key={index}
-			onMouseEnter={(): void => animateLetters(-48)}
-			onMouseLeave={(): void => animateLetters(0)}
+			onMouseEnter={(): void => animateLetters("after")}
+			onMouseLeave={(): void => animateLetters("before")}
 		>
-			{classNames.map((className, i) => (
+			{animations.map((animation, i) => (
 				<Fragment key={i}>
-					<H priority={2} className={className} customStyles={tw`uppercase `}>
+					<MotionHeading
+						priority={2}
+						customStyles={tw`uppercase`}
+						variants={letterContainerVariants}
+						animate={animation}
+					>
 						{linkInner}
-					</H>
+					</MotionHeading>
 				</Fragment>
 			))}
 		</LinkWrapper>
@@ -62,22 +127,14 @@ const FakeLink: FC<FakeLinkProps> = ({ name, index }) => {
 
 const HoverWaveEffect: NextPage = () => {
 	const fakeLinks = [{ name: "Gucci" }, { name: "Balenciaga" }, { name: "Versace" }];
-
-	const LinksContainer = styled.div`
-		${tw`h-screen flex items-center justify-center`}
-		h2 {
-			&:first-of-type {
-				opacity: 0.2;
-			}
-		}
-	`;
+	const LinksContainer = styled.div(() => [tw`h-screen flex items-center justify-center`]);
 
 	return (
 		<Container size="large">
 			<LinksContainer>
 				{fakeLinks.map((link, index) => (
 					<Fragment key={index}>
-						<FakeLink name={link.name} index={index} />
+						<MotionLink name={link.name} index={index} />
 					</Fragment>
 				))}
 			</LinksContainer>
