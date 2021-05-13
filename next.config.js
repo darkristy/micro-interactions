76d471895a38cd3da2b26c515025cf872da9a6cf
Kahlil Whitfield <darkristy2002@gmail.com>
2021-05-12 23:37:36 -0400

    feat(svg): add svg handler

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 module.exports = {
 	future: { webpack5: true },
-	webpack: (config) => {
-		// Unset client-side javascript that only works server-side
-		config.resolve.fallback = { fs: false, module: false };
-
-		return config;
-	},
 };
+
+const path = require("path");
+
+const withPlugins = require("next-compose-plugins");
+const withReactSvg = require("next-react-svg");
+
+module.exports = withPlugins([
+	[
+		withReactSvg,
+		{
+			include: path.resolve(__dirname, "src/svg"),
+			webpack: (config) => {
+				// Unset client-side javascript that only works server-side
+				config.resolve.fallback = { fs: false, module: false };
+				return config;
+			},
+		},
+	],
+	{
+		future: { webpack5: true },
+	},
+]);
